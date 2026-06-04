a78e04841c101497dcd85f41cff09de39b85b7c0
Daniyaal Khalid <daniyaal.kha@gmail.com>
2026-06-04 16:11:26 +1000

    Remove unnecessary database logging

--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -12,8 +12,6 @@ const rootEnvPath = resolve(rootDir, ".env");
 
 loadEnv({ path: rootEnvPath });
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
-
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in the .env file at the root.");
 }
